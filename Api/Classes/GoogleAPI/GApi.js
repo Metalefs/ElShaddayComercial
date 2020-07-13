e4d5c934239a2edbc6dd3442291c3ab8a81c23e5
Jackson <jack-ten@hotmail.com>
2020-07-13 02:29:34 -0300

    working api

--- Api/Classes/GoogleAPI/GApi.js
+++ Api/Classes/GoogleAPI/GApi.js
@@ -1,10 +1,11 @@
 const request = require("request");
+const csv = require('csvtojson')
 class GAPI {
     constructor() {}
      
-    CARDAPIO_ID = '1Rkd3wSvAplfXYWzJT87XhinJMPMpa90VSbI9ku-hOso';
-    INFORMACAO_CONTATO_ID = '1qlU7gm0KsPc2L24J27njmOb4YF3o3xhvUbhjoFLGIC4';
-    SOBRE_ID = '1pLypWkNKRfDFSvocoK82dMJu-TInGe-KkQQRv7RqUQ8';
+    CARDAPIO_ID = '2PACX-1vQXHSq7ZHftNdw0RK28GyIsMKJYDdOaW3GtUEgx5c5YBL8UsLFCnF5CFbkw7Yj3rFx1ax0dDzU8jViv';
+    INFORMACAO_CONTATO_ID = '2PACX-1vSNZ0Y8ioRRGVZUYrPKE3lYr8D3bpk78TK6kvgYdN_AKk33bThMi2hvAgGL9mfKZyM8GATI9wJDkAss';
+    SOBRE_ID = '2PACX-1vThQGr5NLnPFJhNY4rnk6tzYHa75z0_bfekFLSQj_f93GaCUhicHxDfjGvNvlDnZfKqdsZncrOhs-6M';
     
     async LerCardapio(){
         this.ReadSheet(this.CARDAPIO_ID);
@@ -20,8 +21,14 @@ class GAPI {
         let url = `https://docs.google.com/spreadsheets/d/e/${SheetId}/pub?output=csv`;
         request(url, (error, response, body) => {
             if(error) return console.log(error);
-            let json = JSON.parse(body);
-            console.log(json);
+            csv({
+                noheader:true,
+                output: "csv"
+            })
+            .fromString(body)
+            .then((csvRow)=>{ 
+                console.log(csvRow)
+            });
         });     
     }
 }
