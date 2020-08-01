6064f20b00a8084c6b1e783663824b9606c3b10c
Jackson <jack-ten@hotmail.com>
2020-08-01 12:21:19 -0300

    Melhorias pagina entrega mobile

--- Website/src/app/pages/entrega/meus-pedidos/detalhes-pedido/detalhes-pedido.component.ts
+++ Website/src/app/pages/entrega/meus-pedidos/detalhes-pedido/detalhes-pedido.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Collections } from 'src/app/shared/_models/MongoCollections';
+import { PedidoService } from 'src/app/api/services/PedidoService';
 @Component({
   selector: 'app-detalhes-pedido',
   templateUrl: './detalhes-pedido.component.html',
@@ -8,8 +9,10 @@ import { Collections } from 'src/app/shared/_models/MongoCollections';
 export class DetalhesPedidoComponent implements OnInit {
   @Input()
   Pedido:Collections.Pedido;
-  constructor() { }
-
+  constructor(private PedidoService: PedidoService) { }
+  ConfirmarRecebimento(pedido){
+    this.PedidoService.ConfirmarRecebimento(pedido).subscribe();
+  }
   ngOnInit(): void {
   }
 
